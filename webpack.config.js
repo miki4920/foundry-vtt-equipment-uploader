661d15db2de9302ba7e110aaf5d99af9f2afc7b3
miki4920 <miki4920@gmail.com>
2022-09-13 00:36:44 +0100

    Removed need for gulp pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    resolve: {
-        modules: [path.resolve(__dirname, 'node_modules'), 'node_modules']
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
     },
-    entry: '/dist/index.js',
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
