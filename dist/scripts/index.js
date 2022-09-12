082c6855f0572c2da23f6f0d5675536ea4de3083
miki4920 <miki4920@gmail.com>
2022-09-12 22:47:16 +0100

    Added ability to save ids

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,6 +1,5 @@
-import Tagify from '@yaireo/tagify'
-
-Hooks.on('init', async function() {
+import Tagify from '@yaireo/tagify';
+Hooks.on('init', async function () {
     await game["settings"].registerMenu("foundry-vtt-offline-viewer", "foundry-vtt-offline-viewer-settings", {
         name: "Wealth Settings",
         label: "Open Me!",
@@ -9,7 +8,6 @@ Hooks.on('init', async function() {
         type: TagifyInputs,
         restricted: true
     });
-
     await game["settings"].register('foundry-vtt-offline-viewer', 'wealthIDs', {
         scope: 'world',
         config: false,
@@ -19,17 +17,14 @@ Hooks.on('init', async function() {
             "IDs": []
         },
     });
-
 });
-
 class TagifyInputs extends FormApplication {
-    override activateListeners(): void {
-        const priorityInput = document.querySelector(`input[name="priorityWealthInput"]`)
-        const regularInput = document.querySelector(`input[name="wealthInput"]`)
-        new Tagify(priorityInput)
-        new Tagify(regularInput)
+    activateListeners() {
+        const priorityInput = document.querySelector(`input[name="priorityWealthInput"]`);
+        const regularInput = document.querySelector(`input[name="wealthInput"]`);
+        new Tagify(priorityInput);
+        new Tagify(regularInput);
     }
-
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
             classes: ['form'],
@@ -40,38 +35,30 @@ class TagifyInputs extends FormApplication {
             height: 220,
             width: 800,
         });
-
     }
-
-    //
-    protected _updateObject(event: Event, formData?: object): Promise<unknown> {
-        let IDs = {"priorityIDs": [], "IDs": []}
-        if(formData["priorityWealthInput"] !== "") {
+    _updateObject(event, formData) {
+        let IDs = { "priorityIDs": [], "IDs": [] };
+        if (formData["priorityWealthInput"] !== "") {
             for (const element of JSON.parse(formData["priorityWealthInput"])) {
-                IDs["priorityIDs"].push(element["value"])
+                IDs["priorityIDs"].push(element["value"]);
             }
         }
-        if(formData["wealthInput"] !== "") {
+        if (formData["wealthInput"] !== "") {
             for (const element of JSON.parse(formData["wealthInput"])) {
-                IDs["IDs"].push(element["value"])
+                IDs["IDs"].push(element["value"]);
             }
         }
         // @ts-ignore
-        return game.settings.set('foundry-vtt-offline-viewer', 'wealthIDs', IDs)
+        return game.settings.set('foundry-vtt-offline-viewer', 'wealthIDs', IDs);
     }
-
     // @ts-ignore
     getData() {
         // @ts-ignore
         const wealthIDs = game.settings.get('foundry-vtt-offline-viewer', 'wealthIDs');
-        let priorityIDs = wealthIDs["priorityIDs"].join()
-        let IDs = wealthIDs["IDs"].join()
-        return {"IDs": IDs, "priorityIDs": priorityIDs}
+        let priorityIDs = wealthIDs["priorityIDs"].join();
+        let IDs = wealthIDs["IDs"].join();
+        return { "IDs": IDs, "priorityIDs": priorityIDs };
     }
-
-
 }
-
-Hooks.on('ready', async function() {
-
+Hooks.on('ready', async function () {
 });
