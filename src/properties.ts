fa9193b220d0cfdfc424389e801ba1f8d5f2cf35
miki4920 <miki4920@gmail.com>
2022-09-13 18:32:12 +0100

    Added a manual override for rune properties

---
+++ src/properties.ts
@@ -0,0 +1,18 @@
+export const properties = {
+    "armor": {
+        "potencyRune": {
+            1: {
+                "level": 5,
+                "price": 160
+            },
+            2: {
+                "level": 11,
+                "price": 1060
+            },
+            3: {
+                "level": 18,
+                "price": 20560
+            }
+        }
+    }
+}
