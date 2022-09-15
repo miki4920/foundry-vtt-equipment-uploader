b675671f2df87e69c15e2ae572d601e699618b8c
miki4920 <miki4920@gmail.com>
2022-09-15 22:08:06 +0100

    Added insertion of items and characters into the database

--- src/dataHandler.ts
+++ src/dataHandler.ts
@@ -2,14 +2,16 @@ import {properties} from "./properties";
 
 export class DataHandler {
     private players: {};
-    constructor(IDs) {
+    private ddb: any;
+    constructor(IDs, ddb) {
         this.players = this.getPlayers(IDs);
+        this.ddb = ddb
         this.insertIntoDatabase()
     }
 
     getPlayers(IDs) {
         const players = {}
-        for(const key of Object.keys(IDs)) {
+        for (const key of Object.keys(IDs)) {
             players[key] = IDs[key].map(ID => game["actors"].get(ID))
         }
         return players
@@ -17,7 +19,7 @@ export class DataHandler {
 
     getArmourValue(item, armourKey) {
         let value = 0
-        for(const key of Object.keys(properties["armor"])) {
+        for (const key of Object.keys(properties["armor"])) {
             let runes = properties["armor"][key]
             let runeValue = runes[item["system"][key]["value"]]
             runeValue = runeValue === undefined ? 0 : runeValue[armourKey]
@@ -28,7 +30,7 @@ export class DataHandler {
 
     getItemLevel(item) {
         let itemLevel = item["level"]
-        if(item["type"] === "armor") {
+        if (item["type"] === "armor") {
             itemLevel = Math.max(itemLevel, this.getArmourValue(item, "level"))
         }
         return itemLevel
@@ -37,7 +39,7 @@ export class DataHandler {
     getItemValue(item) {
         let value = item["system"]["price"]["value"].copperValue
         value /= 100
-        if(item["type"] === "armor") {
+        if (item["type"] === "armor") {
             value = Math.max(value, this.getArmourValue(item, "price"))
         }
         value /= item["system"]["price"]["per"]
@@ -45,23 +47,43 @@ export class DataHandler {
     }
 
     insertIntoDatabase() {
-        for(const key of Object.keys(this.players)) {
-            for(const player of this.players[key]) {
+        for (const key of Object.keys(this.players)) {
+            for (const player of this.players[key]) {
                 const inventory = player["inventory"].entries();
                 const itemIDs: String[] = [];
                 for (const [key, item] of inventory) {
                     itemIDs.push(item["id"]);
                     const itemName = item["name"];
-                    const itemLevel = this.getItemLevel(item);
-                    const itemValue = this.getItemValue(item);
-                    const itemQuantity = item["system"]["quantity"]
-                    const itemTotal = (itemValue * itemQuantity).toFixed(2);
-                    const itemConsumable = (item["type"] === "consumable") ? 1 : 0;
-                    if(item["type"] === "armor") {
-                        console.log(itemValue)
-                        console.log(itemLevel)
-                    }
+                    const itemLevel = this.getItemLevel(item).toString();
+                    const itemValue = this.getItemValue(item).toString();
+                    const itemQuantity = item["system"]["quantity"].toString();
+                    const itemTotal = (itemValue * itemQuantity).toFixed(2).toString();
+                    const itemConsumable = ((item["type"] === "consumable") ? 1 : 0).toString();
+                    this.ddb.putItem(
+                        {
+                            TableName: 'items',
+                            Item: {
+                                'id': {"S": item["id"]},
+                                'name': {"S": itemName},
+                                'level': {"S": itemLevel},
+                                'value': {"S": itemValue},
+                                'quantity': {"S": itemQuantity},
+                                'total': {"S": itemTotal},
+                                'consumable': {"N": itemConsumable}
+                            }
+                        }, function(err, data) {})
                 }
+                this.ddb.putItem(
+                    {
+                        TableName: 'characters',
+                        Item: {
+                            'id': {"S": player["id"]},
+                            'name': {"S": player["name"]},
+                            'items': {"SS": itemIDs}
+                        }
+                    }
+                ,function(err, data) {}
+                    )
             }
         }
     }
